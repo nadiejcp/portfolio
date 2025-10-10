e2216d8d5b4524cad4a12b88fb241d2d2c36f28f
Jairo Cabrera <jairocabrera87@hotmail.com>
2025-10-10 16:41:07 -0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,10 @@ const nextConfig: NextConfig = {
   output: 'export',
   images: {
     unoptimized: true
-  }
+  },
+  trailingSlash: true,
+  basePath: '/portfolio', // ← Add this line
+  assetPrefix: '/portfolio', // ← And this line
 };
 
 export default nextConfig;
