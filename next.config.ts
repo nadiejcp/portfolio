fdd1accc35228f09f2e320b4582bfad5daa746f5
Jairo Cabrera <jairocabrera87@hotmail.com>
2025-10-10 16:23:28 -0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  distDir: 'out',
-  trailingSlash: true,
-  images: {
-    unoptimized: true
-  },
-  // Optional: Add basePath if deploying to project site (username.github.io/repo-name)
-  // basePath: '/your-repo-name',
-}
+  /* config options here */
+};
 
-module.exports = nextConfig
 export default nextConfig;
