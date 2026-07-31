892f2fca02772cc7705f46ef0afea43f16c1fa19
Jairo Cabrera <63386464+nadiejcp@users.noreply.github.com>
2026-07-31 14:27:02 -0500

    fix lint issues

--- src/app/projects/project/page.tsx
+++ src/app/projects/project/page.tsx
@@ -10,7 +10,6 @@ import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
-import BigSquare from "@/components/BigSquare";
 
 type Testimonial = {
   name: string;
